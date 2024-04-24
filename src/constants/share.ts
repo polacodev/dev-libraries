3614a05c123eabd617af53eca8f0c525c1d9ab46
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2024-04-24 23:54:46 +0200

    Revert "feat: add share button for easy website sharing"
    This reverts commit 96eb90519706a1ae7e657d98d30f2a5a1c2f4886.

--- src/constants/share.ts
+++
@@ -1,5 +0,0 @@
-export const SHARE_DATA: ShareData = {
-  title: 'EasyViewer',
-  url: location.href,
-  text: 'DevLi is a web site to help the engineers and provide the best options of libraries at the moment to work on projects',
-};
--- src/types/notifications.ts
+++
@@ -1,9 +0,0 @@
-export type NotificationType = 'success' | 'error';
-
-export type GradientColors = Record<
-  NotificationType,
-  Record<
-    'firstColor' | 'secondColor' | 'thirdColor',
-    `rgba(${number},${number},${number},${number})`
-  >
->;
