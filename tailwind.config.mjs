7dd35d4c4b634a5c2ae6985ee5305fc47f66a0ab
polacodev <polacodev@gmail.com>
2024-04-29 11:32:40 -0400

    refactor: create colors palette and unify in own file

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,32 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: 'class',
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		// colors: {
-		// 	transparent: 'transparent',
-		// 	current: 'currentColor',
-		// 	'primary-1': '#68a30d',
-		// 	'primary-2': '#baf264',
-		// 	'red': '#be123c',
-		// 	'black': '#000000',
-		// 	// 'background-light': '#efefef',
-		// 	'background-light': '#fcfcfc',
-		// 	'atlantis': {
-		// 		'50': '#f5fee7',
-		// 		'100': '#e9fccb',
-		// 		'200': '#d5f99d',
-		// 		'300': '#baf264',
-		// 		'400': '#a0e635',
-		// 		'500': '#84cc16',
-		// 		'600': '#68a30d',
-		// 		'700': '#517c0f',
-		// 		'800': '#426212',
-		// 		'900': '#3a5314',
-		// 		'950': '#1e2e05',
-		// 	},
-		// },
-		extend: {},
-	},
-	plugins: [],
-}
+  darkMode: 'class',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    colors: {
+      // 'red': '#dc2626', 									// testing
+      'paragraph-link': '#1d4ed8', // text-blue-700	used
+      transparent: 'transparent', // used
+
+      //LIGHT
+      'light-paragraph': '#1f1f29', // used
+      'light-title': '#1F2E05', // used
+      'green-light-primary': '#9ccc16', // used
+      'green-light-secondary': '#E5f99d', // used
+      'light-primary': '#f9fafb', // text-gray-50 used
+      'light-secondary': '#ffffff', // white used
+      'light-border': '#d1d5db', // text-gray-300 used
+
+      //DARK
+      'dark-paragraph': '#e5e7eb', // text-gray-200	used
+      'green-dark-primary': '#BAE635', // used
+      'dark-primary': '#111827', // text-gray-900	used
+      'dark-secondary': '#1f2937', // text-gray-800 used
+      'dark-tertiary': '#374151', // text-gray-700 used
+      'dark-border': '#4b5563', // text-gray-600 used
+      'dark-subtitle': '#9ca3af', // text-gray-400 used
+      'dark-home-card': '#4B4B4B', // used
+    },
+    extend: {},
+  },
+  plugins: [],
+};
