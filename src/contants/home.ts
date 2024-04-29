502a05154a899f27e8e0447664a890472e0dc2f6
polacodev <polacodev@gmail.com>
2024-04-28 20:52:23 -0400

    refactor: home component

---
+++ src/contants/home.ts
@@ -0,0 +1,19 @@
+import { type HomeItem } from '@/types/home.type';
+
+export const HOME_ITEMS: HomeItem[] = [
+  {
+    id: 'ChartJS',
+    name: 'ChartJS',
+    image: '/javascript-typescript/chartjs.svg',
+  },
+  {
+    id: 'NumPy',
+    name: 'NumPy',
+    image: '/python/numpy.svg',
+  },
+  {
+    id: 'Lodash',
+    name: 'Lodash',
+    image: '/javascript-typescript/lodash.svg',
+  },
+];
---
+++ src/types/home.type.ts
@@ -0,0 +1,5 @@
+export interface HomeItem {
+  id: string;
+  name: string;
+  image: string;
+}
